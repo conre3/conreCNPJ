9f7f0d700120cd6c1b589cdc9fc8171cb6037841
Julio Trecenti <julio.trecenti@gmail.com>
2017-01-06 15:53:24 -0200

    conre3 and conre6 docs

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' CONRE-3 registered companies
+#'
+#' A raw dataset containing registered companies scraped from CONRE-3 site.
+#'
+#' @format A data frame with 133 rows and 5 variables:
+#' \describe{
+#'    \item{empresa}{Name of the company.}
+#'    \item{uf}{State initials.}
+#'    \item{cnpj}{Company national id.}
+#' }
+#' @source \url{http://www.conre3.org.br/portal/profissionais-e-empresas-do-conre-3-2/}
+"conre3"
+
+#' CONRE-6 registered companies
+#'
+#' @rdname conre3_companies
+#' @source \url{http://www.conre3.org.br/portal/profissionais-e-empresas-do-conre-3-2/}
+"conre6"
