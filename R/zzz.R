279955945f98210fd960184376b59deb1fcb5802
Julio Trecenti <julio.trecenti@gmail.com>
2017-01-06 15:53:15 -0200

    pipe and global vars

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+globalVariables(c('%>%', '.', 'cnpj', 'uf'))
+
+#' Pipe operator
+#'
+#' See \code{\link[magrittr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
